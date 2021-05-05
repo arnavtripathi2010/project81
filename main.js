43d1824ead7457e37e4bfc663dcc7d1f35173b1f
arnavtripathi2010 <83599145+arnavtripathi2010@users.noreply.github.com>
2021-05-05 10:23:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+ctx.beiginPath();
+ctx.strokStyle = blue;
+ctx.lineWidth = 2;
+ctx.arc(200, 200, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+
+ctx.beiginPath();
+ctx.strokStyle = black;
+ctx.lineWidth = 2;
+ctx.arc(200, 200, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+tx.beiginPath();
+ctx.strokStyle = red;
+ctx.lineWidth = 2;
+ctx.arc(200, 200, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+tx.beiginPath();
+ctx.strokStyle = yellow;
+ctx.lineWidth = 2;
+ctx.arc(200, 200, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+tx.beiginPath();
+ctx.strokStyle = green;
+ctx.lineWidth = 2;
+ctx.arc(200, 200, 40, 0, 2 * Math.PI);
+ctx.stroke();
